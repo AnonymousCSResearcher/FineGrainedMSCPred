6c438294d750e5840456a5c647031a05e1ba00e7
AnonymousCSResearcher <85504774+AnonymousCSResearcher@users.noreply.github.com>
2024-05-17 13:33:08 +0200

    Add api updates (postreq, preproc, fixes)

--- api/api.py
+++ api/api.py
@@ -1,58 +1,111 @@
+from pathlib import Path
 from fastapi import FastAPI, Query
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 import uvicorn
 import modules
 
+class TextRequest(BaseModel):
+	text: str
+	preproc: str = None
 
 # Create an instance of the FastAPI class
 app = FastAPI()
 # Mount the "static" directory to serve static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
+try:
+	# Docker container
+	app.mount("/static", StaticFiles(directory="static"), name="static")
+except:
+	# Local testing
+	root_path = Path(__file__).resolve().parent.parent
+	# Mount the root directory to serve static files
+	app.mount("/static", StaticFiles(directory=root_path), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def route_example():
-    return '''
-        <html>
-        <head>
-            <title>zbMATH Fine-Grained MSC Prediction API</title>
-            <link rel="shortcut icon" type="image/png" href="/static/favicon_zbMATH.png"/>
-        </head>
-        <body>
-            <h1>Fine-Grained MSC Prediction API</h1>
-            <img src="/static/Logo_zbMATH-Open-201223.svg" alt="zbMATH" width="200">
-            <h3>Example Queries</h3>
-            <a href="/predict_text_keywords_qids?text=Summary:%20This%20paper%20presents%20a%20novel%20path%20integral%20formalism%20for%20Einstein%E2%80%99s%20theory%20of%20gravitation%20from%20%27%20\%27the%20viewpoint%20of%20optimal%20control%20theory.%20Despite%20its%20close%20connection%20to%20the%20well-known%20variational%20%27%20\%27principle%20of%20physicists,%20optimal%20control%20turns%20out%20to%20be%20more%20general.%20Within%20this%20context,%20a%20Lagrangian%20%27%20\%27which%20is%20different%20from%20the%20Einstein-Hilbert%20Lagrangian%20is%20defined.%20Einstein%E2%80%99s%20field%20equations%20are%20%27%20\%27recovered%20exactly%20with%20variations%20of%20the%20new%20action%20functional.%20The%20quantum%20theory%20is%20obtained%20using%20%27%20\%27Ashtekar%20variables%20and%20the%20loop%20scalar%20product.%20As%20an%20illustrative%20example,%20the%20tunneling%20process%20of%20a%20%27%20\%27black%20hole%20into%20another%20black%20hole%20or%20into%20a%20white%20hole%20is%20investigated%20with%20a%20toy%20model.">Text to Keywords</a>
-            <a href="/predict_text_mscs?text=Summary:%20This%20paper%20presents%20a%20novel%20path%20integral%20formalism%20for%20Einstein%E2%80%99s%20theory%20of%20gravitation%20from%20%27%20\%27the%20viewpoint%20of%20optimal%20control%20theory.%20Despite%20its%20close%20connection%20to%20the%20well-known%20variational%20%27%20\%27principle%20of%20physicists,%20optimal%20control%20turns%20out%20to%20be%20more%20general.%20Within%20this%20context,%20a%20Lagrangian%20%27%20\%27which%20is%20different%20from%20the%20Einstein-Hilbert%20Lagrangian%20is%20defined.%20Einstein%E2%80%99s%20field%20equations%20are%20%27%20\%27recovered%20exactly%20with%20variations%20of%20the%20new%20action%20functional.%20The%20quantum%20theory%20is%20obtained%20using%20%27%20\%27Ashtekar%20variables%20and%20the%20loop%20scalar%20product.%20As%20an%20illustrative%20example,%20the%20tunneling%20process%20of%20a%20%27%20\%27black%20hole%20into%20another%20black%20hole%20or%20into%20a%20white%20hole%20is%20investigated%20with%20a%20toy%20model.">Text to MSCs</a>
-            <a href="/predict_keyword_mscs?keywords=path%20integral%20formalism,variational%20principle,optimal%20control,process%20of,black%20hole,principle%20of,Einstein-Hilbert%20Lagrangian,Ashtekar%20variables,theory%20of%20gravitation,tunneling%20process,white%20hole,path%20integral,quantum%20theory,field%20equations,integral%20formalism,theory%20of">Keywords to MSCs</a>
-        </body>
-        </html>
-        '''
+
+	text = """
+		Summary:%20This%20paper%20presents%20a%20novel%20path%20integral%20formalism%20for%20Einstein%E2%80%99s%20theory%20of%20gravitation%20from%20the%20viewpoint%20of%20optimal%20control%20theory.%20Despite%20its%20close%20connection%20to%20the%20well-known%20variational%20principle%20of%20physicists,%20optimal%20control%20turns%20out%20to%20be%20more%20general.%20Within%20this%20context,%20a%20Lagrangian%20which%20is%20different%20from%20the%20Einstein-Hilbert%20Lagrangian%20is%20defined.%20Einstein%E2%80%99s%20field%20equations%20are%20recovered%20exactly%20with%20variations%20of%20the%20new%20action%20functional.%20The%20quantum%20theory%20is%20obtained%20using%20Ashtekar%20variables%20and%20the%20loop%20scalar%20product.%20As%20an%20illustrative%20example,%20the%20tunneling%20process%20of%20a%20black%20hole%20into%20another%20black%20hole%20or%20into%20a%20white%20hole%20is%20investigated%20with%20a%20toy%20model.
+	"""
+
+	return f'''
+		<html>
+		<head>
+			<title>zbMATH Fine-Grained MSC Prediction API</title>
+			<link rel="shortcut icon" type="image/png" href="/static/favicon_zbMATH.png"/>
+		</head>
+		<body>
+			<h1>Fine-Grained MSC Prediction API</h1>
+			<img src="/static/Logo_zbMATH-Open-201223.svg" alt="zbMATH" width="200">
+			<h3>Example Queries</h3>
+			<a href="/predict_text_keywords_qids?text={text}">Text to Keywords</a>
+			<a href="/predict_text_keywords_augmented?text={text}&preproc=none">Text to Wikipedia*</a>
+			<a href="/predict_linked_text_keywords?text={text}&preproc=none">Text with Wikipedia Links*</a>
+			<a href="/predict_text_mscs?text={text}">Text to MSCs</a>
+			<a href="/predict_keyword_mscs?keywords=path integral formalism,variational principle,optimal control,process of,black hole,principle of,Einstein-Hilbert Lagrangian,Ashtekar variables,theory of gravitation,tunneling process,white hole,path integral,quantum theory,field equations,integral formalism,theory of">Keywords to MSCs</a>
+			<p>*Use parameter 'preproc' in ['none', 'stem', 'lemma']</p>
+		</body>
+		</html>
+	'''
+
+
+# KEYWORD EXTRACTION
+
 
 # Predict keywords and Wikidata QIDs from (abstract) text
 @app.get("/predict_text_keywords_qids")
-def route_predict_text_keywords_qids(text: str):
-    keywords, qids = modules.extract_keywords(text)
-    return {"keywords": keywords, "qids": qids}
+async def route_predict_text_keywords_qids(text: str):
+	keywords, qids = modules.extract_keywords(text)
+	keyword_qid = [{"entity": keyword_qid[0], "qid": keyword_qid[1]} for keyword_qid in zip(keywords, qids)]
+	return {"keyword_qid": keyword_qid} # {"keywords": keywords, "qids": qids}
+
+
+# Predict keywords from (abstract) text augmented with spans, Wikidata QIDs and Wikipedia URLs
+@app.get("/predict_text_keywords_augmented")
+async def get_predict_text_keywords_augmented(text: str, preproc: str = None):
+	keywords_augmented = modules.extract_keywords_augmented(text, preproc)
+	return {'keyword_span_qid_url': keywords_augmented}
+
+
+@app.post("/predict_text_keywords_augmented")
+async def post_predict_text_keywords_augmented(request: TextRequest):
+	keywords_augmented = modules.extract_keywords_augmented(request.text, request.preproc)
+	return {'keyword_span_qid_url': keywords_augmented}
+
+
+# Predict keywords from (abstract) text and return a linked html text
+@app.get("/predict_linked_text_keywords", response_class=HTMLResponse)
+async def route_predict_linked_text_keywords(text: str, preproc: str = None):
+	linked_text_html = modules.get_linked_text_html(text, preproc)
+	linked_text_full_html = f'''<html><body>{linked_text_html}</body></html>'''
+	#return {'linked_text_html': linked_text_html}
+	return linked_text_full_html
+
+
+# MSC PREDICTION
 
 
 # Predict MSCs from (abstract) text
 @app.get("/predict_text_mscs")
-def route_predict_text_mscs(text: str):
-    keywords, _ = modules.extract_keywords(text)
-    mscs = modules.predict_mscs(keywords)
-    return {"mscs": mscs}
+async def route_predict_text_mscs(text: str):
+	keywords, _ = modules.extract_keywords(text)
+	mscs = modules.predict_mscs(keywords)
+	return {"mscs": mscs}
 
 
 # Predict MSCs from keywords
 @app.get("/predict_keyword_mscs")
-def route_predict_keyword_mscs(keywords: str = Query(..., title="keywords")):
-    # Parse the comma-separated string into a list
-    keywords = [kw.strip() for kw in keywords.split(",")]
-    mscs = modules.predict_mscs(keywords)
-    return {"mscs": mscs}
+async def route_predict_keyword_mscs(keywords: str = Query(..., title="keywords")):
+	# Parse the comma-separated string into a list
+	keywords = [kw.strip() for kw in keywords.split(",")]
+	mscs = modules.predict_mscs(keywords)
+	return {"mscs": mscs}
+
+
+# TODO: Predict keywords from MSCs
 
 
 # Run the application using the FastAPI's included "uvicorn.run" function
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+	uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
--- api/modules.py
+++ api/modules.py
@@ -1,7 +1,10 @@
+import os
 import json
 import nltk
 nltk.download('stopwords')
-from nltk import ngrams
+nltk.download('wordnet')
+from nltk import ngrams, PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords as stopwords_nltk
 stopwords_nltk = stopwords_nltk.words('english')
 import pywikibot
@@ -18,147 +21,275 @@ nr_keywords_cutoff = 5
 # from evaluate_classification import load_index
 def load_index():
 
-    # ent_cls_idx
-    print('Load entity-class index...')
-    with open("./data/ent_cls_idx.json", 'r') as f:
-        sorted_ent_cls_idx = json.load(f)
-    print('...done!')
+	# ent_cls_idx
+	print('Load entity-class index...')
+	try:
+		# Docker container
+		with open("./data/ent_cls_idx.json", 'r', encoding='utf-8') as f:
+			sorted_ent_cls_idx = json.load(f)
+	except:
+		# Local testing
+		with open(os.path.join(os.path.dirname(os.getcwd()), 'data/ent_cls_idx.json'),
+				  'r', encoding='utf-8') as f:
+			sorted_ent_cls_idx = json.load(f)
+	print('...done!')
 
-    return sorted_ent_cls_idx
+	return sorted_ent_cls_idx
 
 
 # get Wikidata qid from name using pywikibot
 def get_qid_pywikibot(name):
-    try:
-        site = pywikibot.Site("en", "wikipedia")
-        page = pywikibot.Page(site, name)
-        item = pywikibot.ItemPage.fromPage(page)
-        qid = item.id
-    except:
-        qid = None
-    return qid
+	try:
+		site = pywikibot.Site("en", "wikipedia")
+		page = pywikibot.Page(site, name)
+		item = pywikibot.ItemPage.fromPage(page)
+		qid = item.id
+	except:
+		qid = None
+	return qid
+
+def get_wikidata_qid_wikipedia_url(name):
+
+	verbose = False
+
+	qid = None
+	url = None
+
+	try:
+
+		# get wikidata qid
+		lang = 'en'
+		site = pywikibot.Site(lang, 'wikipedia')
+		page = pywikibot.Page(site, name)
+		item = pywikibot.ItemPage.fromPage(page)
+		qid = item.id
+
+		# get wikipedia url
+		try:
+			wiki_code = lang + 'wiki'
+			page_title = item.sitelinks[wiki_code].title
+			# Create the URL manually
+			url = f"https://en.wikipedia.org/wiki/{page_title.replace(' ', '_')}"
+		except Exception as e:
+			if verbose:
+				print(f'Error: {e}')
+
+	except Exception as e:
+		if verbose:
+			print(f'Error: {e}')
+
+	return qid, url
+
 
 
 def get_keywords(text, get_qids):
-    with open("./stopwords.txt", 'r') as f:
-        stopwords_custom = f.readlines()
 
-    keywords = []
-    qids = []
+	keywords = []
+	qids = []
 
-    n_gram_lengths = [2, 3]
-    for n in n_gram_lengths:
-        try:
-            nngrams = ngrams(text.split(), n)
-            for nngram in nngrams:
-                entity = ''
-                for word in nngram:
-                    entity += word + ' '
-                entity = entity[:-1]
-                try:
-                    if sorted_ent_cls_idx[entity] is not None and entity not in stopwords_nltk and entity not in stopwords_custom:
-                        keywords.append(entity)
-                except:
-                    pass
-        except:
-            pass
+	n_gram_lengths = [2, 3]
+	for n in n_gram_lengths:
+		try:
+			nngrams = ngrams(text.split(), n)
+			for nngram in nngrams:
+				entity = ''
+				for word in nngram:
+					entity += word + ' '
+				entity = entity[:-1]
+				try:
+					if sorted_ent_cls_idx[entity] is not None and entity not in stopwords_nltk and entity not in stopwords_custom:
+						keywords.append(entity)
+				except:
+					pass
+		except:
+			pass
 
-    keywords = list(set(keywords))
+	keywords = list(set(keywords))
 
-    # qid via pywikibot
-    if get_qids:
-        for keyword in keywords:
-            qids.append(get_qid_pywikibot(keyword))
+	# qid via pywikibot
+	if get_qids:
+		for keyword in keywords:
+			qids.append(get_qid_pywikibot(keyword))
 
-    return keywords, qids
+	return keywords, qids
 
+def get_keywords_spans_qids_wikip(text, preprocessing = None):
+
+	# Preprocessing
+
+	if preprocessing == 'stem':
+		preprocesser = PorterStemmer()
+	elif preprocessing == 'lemma':
+		preprocesser = WordNetLemmatizer()
+
+	verbose = False
+
+	keywords = []
+	keywords_augmented = []
+
+	# Tokenize the text once (needed for accurate index tracking)
+	tokens = text.split()
+
+	n_gram_lengths = [2, 3]
+	for n in n_gram_lengths:
+		# Generate the n-grams from tokens
+		current_ngrams = list(ngrams(tokens, n))
+
+		# Loop through each n-gram to find the valid ones
+		for index, nngram in enumerate(current_ngrams):
+
+			# Identify stemming/lemmatization differences
+			# if verbose:
+			# 	for word in nngram:
+			# 		if preprocessing == 'stem':
+			# 			preprocessed = preprocesser.stem(word)
+			# 		elif preprocessing == 'lemma':
+			# 			preprocessed = preprocesser.lemmatize(word)
+			# 		if preprocessed != word:
+			# 			keywords_augmented.append(('preprocessed: ' + word + ' -> ' + preprocessed, (None, None), None, None))
+
+			# Create the entity from words in n-gram
+			if preprocessing in [None, 'None', 'none', 'null']:
+				entity = ' '.join(nngram)
+			if preprocessing == 'stem':
+				entity = ' '.join([preprocesser.stem(word) for word in nngram])
+			elif preprocessing == 'lemma':
+				entity = ' '.join([preprocesser.lemmatize(word) for word in nngram])
+
+			# Check the entity against the conditions provided
+			try:
+				if not entity in keywords and \
+						sorted_ent_cls_idx[entity] is not None and \
+						entity not in stopwords_nltk and \
+						entity not in stopwords_custom \
+						and entity.split()[-1] not in ['of']:
+					# Calculate the span of the entity in the text
+					start_index = text.find(entity)
+					end_index = start_index + len(entity) if start_index != -1 else -1
+					# Search for Wikidata QID and Wikipedia URL
+					qid, url = get_wikidata_qid_wikipedia_url(entity)
+					# Only store the entity if it has a Wikipedia URL
+					if url is not None:
+						# Store the entity (to prevent duplicates) and its augmentations (tupel)
+						keywords.append(entity)
+						#keywords_augmented.append((entity, (start_index, end_index), qid, url))
+						keywords_augmented.append({'entity': entity, 'span': [start_index, end_index], 'qid': qid, 'link': url})
+			except Exception as e:
+				if verbose:
+					print(f'Error: {e}')
+
+	return keywords_augmented
+
+def get_linked_text_html(text, preprocessing):
+
+	keywords_augmented = get_keywords_spans_qids_wikip(text, preprocessing)
+
+	linked_text_html = text
+
+	for keyword in keywords_augmented:
+		entity = keyword['entity']
+		link = keyword['link']
+		if link is not None:
+			linked_text_html = linked_text_html.replace(entity, f'<a href="{link}">{entity}</a>')
+
+	return linked_text_html
 
 def get_mscs(keywords):
-    mscs_predicted_single = []
-    mscs_predicted_stat = {}
+	mscs_predicted_single = []
+	mscs_predicted_stat = {}
 
-    for entity in keywords:
-        try:
-            mscs_predicted_single.extend(list(sorted_ent_cls_idx[entity])[0:1])
-            for cls in sorted_ent_cls_idx[entity].items():
-                try:
-                    # SELECTION HERE
-                    mscs_predicted_stat[
-                        cls[0]] += 1  # cls[1]#1 # weightedcontribution or binarycontribution
-                except:
-                    mscs_predicted_stat[cls[0]] = 1
-        except:
-            pass
+	for entity in keywords:
+		try:
+			mscs_predicted_single.extend(list(sorted_ent_cls_idx[entity])[0:1])
+			for cls in sorted_ent_cls_idx[entity].items():
+				try:
+					# SELECTION HERE
+					mscs_predicted_stat[
+						cls[0]] += 1  # cls[1]#1 # weightedcontribution or binarycontribution
+				except:
+					mscs_predicted_stat[cls[0]] = 1
+		except:
+			pass
 
-    # sort
-    sorted_mscs_predicted_stat = dict(
-        sorted(mscs_predicted_stat.items(), key=lambda item: item[1], reverse=True))
+	# sort
+	sorted_mscs_predicted_stat = dict(
+		sorted(mscs_predicted_stat.items(), key=lambda item: item[1], reverse=True))
 
-    return mscs_predicted_single, sorted_mscs_predicted_stat
+	return mscs_predicted_single, sorted_mscs_predicted_stat
 
 
 def extract_keywords(text):
 
-    print('Extracting keywords...')
+	print('Extracting keywords...')
 
-    # get keywords
-    keywords, qids = get_keywords(text, get_qids=True)
-    if len(keywords) == 0:
-        print('Could not extract keywords')
-    else:
-        try:
-            print('Extracted keywords')
-            # print keywords
-            qids_url_prefix = 'https://www.wikidata.org/wiki/'  # e.g., Q11412
-            qids_url_string = ''
-            i = 0
-            for keyword in keywords:
-                try:
-                    qid = qids[i]
-                    if qid is not None:
-                        qids_url_string += f'<a href="{qids_url_prefix + qid}">{keyword}</a>, '
-                    else:
-                        qids_url_string += keyword + ', '
-                except:
-                    qids_url_string += keyword + ', '
-                # print(keyword)
-                i += 1
-            print(qids_url_string.rstrip(', '))
-        except:
-            print('Could not extract keywords')
+	# get keywords
+	keywords, qids = get_keywords(text, get_qids=True)
 
-    return keywords, qids
+	if len(keywords) == 0:
+		print('Could not extract keywords')
+	else:
+		try:
+			print('Extracted keywords')
+			# print keywords
+			qids_url_prefix = 'https://www.wikidata.org/wiki/'  # e.g., Q11412
+			qids_url_string = ''
+			i = 0
+			for keyword in keywords:
+				try:
+					qid = qids[i]
+					if qid is not None:
+						qids_url_string += f'<a href="{qids_url_prefix + qid}">{keyword}</a>, '
+					else:
+						qids_url_string += keyword + ', '
+				except:
+					qids_url_string += keyword + ', '
+				# print(keyword)
+				i += 1
+			print(qids_url_string.rstrip(', '))
+		except:
+			print('Could not extract keywords')
 
+	return keywords, qids
+
+
+def extract_keywords_augmented(text, preprocessing):
+
+	keywords_with_spans = get_keywords_spans_qids_wikip(text, preprocessing)
+
+	return keywords_with_spans
 
 def predict_mscs(keywords):
 
-    print('Predicting MSCs...')
+	print('Predicting MSCs...')
 
-    # get mscs
-    # keywords,qids = get_keywords(text,get_qids=True)
-    mscs_predicted_single, sorted_mscs_predicted_stat = get_mscs(keywords)
-    mscs = list(sorted_mscs_predicted_stat)[:nr_mscs_cutoff]  # mscs_predicted_single[:nr_mscs_cutoff]
-    if len(mscs) == 0:
-        print('Could not predict MSCs')
-    else:
-        try:
-            print('Predicted MSCs')
-            # print mscs
-            # mscs_url_prefix = 'https://mathscinet.ams.org/mathscinet/msc/msc2010.html?t=' #e.g., 81V17
-            mscs_url_prefix = 'https://zbmath.org/classification/?q=cc:'
-            mscs_url_string = ''
-            for msc in mscs:
-                mscs_url_string += f'<a href="{mscs_url_prefix + msc}">{msc}</a>, '
-                # print(msc)
-            print(mscs_url_string.rstrip(', '))
-        except:
-            print('Could not predict MSCs')
+	# get mscs
+	# keywords,qids = get_keywords(text,get_qids=True)
+	mscs_predicted_single, sorted_mscs_predicted_stat = get_mscs(keywords)
+	mscs = list(sorted_mscs_predicted_stat)[:nr_mscs_cutoff]  # mscs_predicted_single[:nr_mscs_cutoff]
+	if len(mscs) == 0:
+		print('Could not predict MSCs')
+	else:
+		try:
+			print('Predicted MSCs')
+			# print mscs
+			# mscs_url_prefix = 'https://mathscinet.ams.org/mathscinet/msc/msc2010.html?t=' #e.g., 81V17
+			mscs_url_prefix = 'https://zbmath.org/classification/?q=cc:'
+			mscs_url_string = ''
+			for msc in mscs:
+				mscs_url_string += f'<a href="{mscs_url_prefix + msc}">{msc}</a>, '
+				# print(msc)
+			print(mscs_url_string.rstrip(', '))
+		except:
+			print('Could not predict MSCs')
 
-    return mscs
+	return mscs
 
-        
+
 # EXECUTE
 
+# load stopwords
+with open("./stopwords.txt", 'r') as f:
+	stopwords_custom = f.readlines()
 
 # load index
 print('Loading indexes...')
